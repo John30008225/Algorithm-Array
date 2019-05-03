6684fee783d9c648a0e9da936f7c4f0345a1137f
John Elder <30008225@student.toiohomai.ac.nz>
2019-05-03 14:44:02 +1200

    Algorithm

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] eg1 = new int[100000];
+            Random randNum = new Random();
+
+            for (int i = 0; i < eg1.Length; i++)
+            {
+                Console.Write((eg1[i] = randNum.Next(100, 999)) + " ");
+                
+                {
+                    Console.WriteLine();
+                }
+            }
+            Console.ReadLine();
+
+
+            Stopwatch = new Stopwatch
+        }
+    }
+}
